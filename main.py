62acdec69c86098d0a19ee23342914c79924f8dd
hongyaa <56691368+hongyaa@users.noreply.github.com>
2019-10-17 13:30:00 -0400

    init die ui

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import test
+import dice
 
+
+d = dice.Dice();
+print('Welcome to Monopoly, let\'s roll the die!')
+roll1 = d.roll();
+roll2 = d.roll();
+print('you rolled: '+str(roll1)+' and '+str(roll2))
+print('you moved '+ str(roll1+roll2)+' spaces')
