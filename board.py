9a823067eab1d964c61d876da365001a880ecd5c
nick postma <nick.postma@l3harris.com>
2019-10-17 14:08:48 -0400

    added move functionality

--- board.py
+++ board.py
@@ -13,7 +13,7 @@ class test(unittest.TestCase):
         board.assign_property_to_dictionary(property)
         self.assertEqual(board.property_dictionary[0] , property)
         
-    def test_set_player_starting_position(self): 
+    def test_set_player_position(self): 
         
         player = Player_test(1) 
         property = Property_test(index = 0)
@@ -21,7 +21,7 @@ class test(unittest.TestCase):
         index = 0 
         
         board.assign_property_to_dictionary(property)
-        board.set_player_starting_position(player, index) 
+        board.set_player_position(player, index) 
         self.assertEqual(board.property_dictionary[0].players[0], player)
         
     def test_set_all_players_starting_position(self): 
@@ -47,7 +47,7 @@ class test(unittest.TestCase):
         
         self.assertEqual(board.get_players_at_index(index), player_set)
     
-    def test_find_player_by_id(self): 
+    def test_get_player_by_id(self): 
         player_set = [Player_test(i) for i in range(5)]
         property = Property_test(index = 0)
         board = Board() 
@@ -57,7 +57,27 @@ class test(unittest.TestCase):
         board.set_all_players_starting_position(player_set, index)
         specific_player_id = 1 
         
-        self.assertEqual(board.find_player_by_id(specific_player_id), player_set[1])
+        self.assertEqual(board.get_player_by_id(specific_player_id)
+                        , (player_set[1], index))
+        
+    
+    def test_remove_player_from_property(self): 
+    
+        player_set = [Player_test(i) for i in range(5)]
+        property = Property_test(index = 0)
+        board = Board() 
+        index = 0       
+        board.assign_property_to_dictionary(property)
+        board.set_all_players_starting_position(player_set, index)
+        specific_player_id = 1 
+        
+        expected_players_at_property = [player for player in player_set if player.id != specific_player_id]
+        
+        #returns same as get player 
+        self.assertEqual(board.get_player_by_id(specific_player_id, remove = True), (player_set[1], index))
+        
+        #property at index no longer contains that player
+        self.assertEqual(board.property_dictionary[index].players, expected_players_at_property)
         
         
     
@@ -81,8 +101,37 @@ class test(unittest.TestCase):
         
         board.move_player_by_player_id(specific_player_id, move_spaces) 
         
+        self.assertEqual(len([player for player in board.property_dictionary[index].players 
+                        if player.id == specific_player_id]), 0)
         
-    
+        self.assertEqual(board.get_player_by_id(specific_player_id)
+                            , (player_set[2], destination_property.index))
+        
+    def test_move_specific_player_past_index_39(self): 
+        
+        player_set = [Player_test(i) for i in range(5)]
+        
+        property = Property_test(index = 0)
+        destination_property = Property_test(index = 1)
+        
+        board = Board() 
+        index = 39     
+        specific_player_id = 2 
+        move_spaces = 2
+        
+        board.assign_property_to_dictionary(property)
+        board.assign_property_to_dictionary(destination_property)
+        
+        
+        board.set_all_players_starting_position(player_set, index)
+        
+        board.move_player_by_player_id(specific_player_id, move_spaces) 
+        
+        self.assertEqual(len([player for player in board.property_dictionary[index].players 
+                        if player.id == specific_player_id]), 0)
+        
+        self.assertEqual(board.get_player_by_id(specific_player_id)
+                            , (player_set[2], destination_property.index))
     pass 
     
     
@@ -106,24 +155,47 @@ class Board():
     def _assemble_property_dictionary(self): 
         for i in range(40): 
             self.property_dictionary[i] = None
-    
+        
+        for i in range(40):
+            self.assign_property_to_dictionary(Property_test(index = i))
+        
     def assign_property_to_dictionary(self, property):
         self.property_dictionary[property.index] = property
         
-    def set_player_starting_position(self, player, index): 
+    def set_player_position(self, player, index): 
         self.property_dictionary[index].players.append(player) 
         
     def set_all_players_starting_position(self, players, index): 
         for player in players: 
-            self.set_player_starting_position(player, index) 
+            self.set_player_position(player, index) 
     
     def get_players_at_index(self, index): 
         return self.property_dictionary[index].players 
     
-    
+    def get_player_by_id(self, player_id, remove = False): 
+        for index, property in self.property_dictionary.items(): 
+
+            for player in property.players:
+                if player.id == player_id:
+                    if not remove: 
+                        return (player, index)
+                    else: 
+                        p = property.players.remove(player)
+                        return (player, index) 
+            
+        assert 1 == 0, 'Player Not Found in Any Property on Board'
+            
     
     def move_player_by_player_id(self, player_id, move_amount): 
         
+        player, player_index = self.get_player_by_id(player_id, remove = True)
+        
+        new_index = player_index + move_amount
+        if new_index >= 40:
+            new_index -= 40 
+            
+        self.set_player_position(player, new_index)
+    
         return 
     
     pass 
